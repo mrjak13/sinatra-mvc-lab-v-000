4d549d00ffa68bca00ba400eeca7acc5b7b0e6aa
Justin Kenney <justinkenney13@gmail.com>
2018-11-11 16:05:14 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,23 @@ class PigLatinizer
     @text = text
   end
 
-  def piglatinize(word)
+  def piglatinize(text)
+    binding.pry
+
+    # vowles text.scan(/[aeoui]/)
+    # consonants text.scan(/[bcdfghjklmnpqrstvwxyz]/)
+    word.split.collect do |word|
+      if word[0].include?(/[aeoui]/)
+        word << 'ay'
+      elsif word[0].include?('/[bcdfghjklmnpqrstvwxyz]/') && word[1].include?(/[bcdfghjklmnpqrstvwxyz]/)
+        letters = word[0, 2]
+        word.gsub(word[0, 2], "")
+      end
+    end
+
+
+
 
   end
+
 end
